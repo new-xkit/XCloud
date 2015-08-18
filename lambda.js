bcbbee6a3858eb400cc141af8b74fffb780cf772
ChuckL <cstebs@gmail.com>
2015-08-18 17:26:05 -0500

    Working on setting up lambda routes.

--- lambda.js
+++ lambda.js
@@ -2,7 +2,20 @@
  * Endpoints for AWS Lambda
  * Created by Chuck on 2015-08-18.
  */
+
 "use strict";
+
+var AWS = require('aws-sdk');
+var bcrypt = require('bcryptjs');
+var uuid = require('node-uuid');
+var auth = require('basic-auth');
+
+//Load routes for v1 XCloud
+var repo = require('./v1/aws-repo.js')(AWS, bcrypt, uuid);
+//var repo = require('./v1/mock-repo.js')();
+var services = require('./v1/services.js')(repo);
+
+
 exports.fetch = function(event, context) {
 	var authorization = auth(event.headers) === undefined ? {name: null, pass: null} : auth(event.headers);
 
